7b430e2f71c3fdc3817cad69b0b3db2928cc9dfd
Abhik004 <abhiksamanta004@gmail.com>
2025-09-29 11:58:57 +0530

    Enrolled_skills_marked_as_“Enrolled”_when_opened

--- server/routes/enrollmentRoutes.js
+++ server/routes/enrollmentRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { completeEnrollment, getEnrolledSkills } = require('../controllers/enrollmentController');
+const { completeEnrollment, getEnrolledSkills,checkEnrollmentStatus } = require('../controllers/enrollmentController');
 
 const router = express.Router();
 
 router.post('/complete', completeEnrollment);
 router.get('/my-skills', getEnrolledSkills);
+router.get('/check/:skillId', checkEnrollmentStatus);
 
 module.exports = router;
